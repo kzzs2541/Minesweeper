d71c60d20ea6691343c4d2764617837663fd50bc
kzzs <01197783@pw.edu.pl>
2024-12-23 13:33:50 +0100

    dodanie wyboru poziomu trudności

--- src/main.c
+++ src/main.c
@@ -3,13 +3,37 @@
 #include <stdbool.h>
 #include "operands.h"
 
-
-
-
 int main(int argc, char **argv)
 {
-    int width = atoi(argv[1]);
-    int height = atoi(argv[2]);
+    system("clear");
+    int width, height, mines;
+    printf("Podaj poziom trudności: 1 - łatwy; 2 - średni; 3 - trudny\n");
+    int x;
+    scanf("%d", &x);
+    while(!(x == 1 || x == 2 || x == 3))
+    {
+        system("clear");
+        printf("Podaj poziom trudności: 1 - łatwy; 2 - średni; 3 - trudny\n");
+        scanf("%d", &x);
+    }
+    if(x == 1)
+    {
+        width = 9;
+        height = 9;
+        mines = 10;
+    }
+    else if(x == 2)
+    {
+        width = 16;
+        height = 16;
+        mines = 40;
+    }
+    else
+    {
+        width = 30;
+        height = 16;
+         mines = 99;
+    }
 
     tile *playField = createTiles(width, height);
 
