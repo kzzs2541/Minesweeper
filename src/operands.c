566f9ffbfe940fe4d066260118244d902960dfbd
kzzs <01197783@pw.edu.pl>
2024-12-30 15:03:09 +0100

    funkcja rysująca plansze, stawiajaca miny, wyswietlająca stan planszy po przegraniu-wyswietla wszystkie miny

--- src/operands.c
+++ src/operands.c
@@ -1,20 +1,105 @@
 #include "operands.h"
 #include <stdbool.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
-tile *createTiles(int width, int height)
+tile **createTiles(int width, int height)
 {
-    tile* playField = malloc(width * height * sizeof(tile));
-    for(int i = 1; i <= height; i++)
+    tile **playField = malloc(height * sizeof(tile *));
+    for (int i = 0; i < height; i++)
     {
-        for(int j = 1; j <= width; j++)
+        playField[i] = malloc(width * sizeof(tile));
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
         {
-            playField[(i-1) * width + (j-1)].x = j; 
-            playField[(i-1) * width + (j-1)].y = i;
-            playField[(i-1) * width + (j-1)].isFlagged = false;
-            playField[(i-1) * width + (j-1)].isMine = false;
-            playField[(i-1) * width + (j-1)].minesAround = 0;
+            playField[i][j].isFlagged = false;
+            playField[i][j].isMine = false;
+            playField[i][j].minesAround = 0;
+            playField[i][j].isRevealed = false;
         }
     }
     return playField;
+}
+
+void printBoard(int height, int width, tile **playField)
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            /*if (playField[i][j].isMine)
+            {
+                printf("M ");
+            }*/
+            if (!playField[i][j].isRevealed)
+            {
+                printf("■ ");
+            }
+            if (playField[i][j].isRevealed && playField[i][j].minesAround == 0)
+            {
+                printf("□ ");
+            }
+            if (playField[i][j].isRevealed && playField[i][j].isFlagged)
+            {
+                printf("F ");
+            }
+            if (playField[i][j].isRevealed && playField[i][j].minesAround != 0)
+            {
+                printf("%d ", playField[i][j].minesAround);
+            }
+        }
+        printf("\n");
+    }
+}
+
+void placeMines(int width, int height, int mines, tile **playField)
+{
+    int placedMines = 0;
+    int pickX, pickY;
+    while (placedMines < mines)
+    {
+        bool canBePlaced = true;
+        pickX = rand() % width;
+        pickY = rand() % height;
+        if (!playField[pickY][pickX].isMine)
+        {
+            playField[pickY][pickX].isMine = true;
+            placedMines++;
+        }
+    }
+}
+
+void revealMines(int width, int height, tile **playField)
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            if (playField[i][j].isMine)
+            {
+                printf("M ");
+            }
+            else if (!playField[i][j].isRevealed)
+            {
+                printf("■ ");
+            }
+            if (playField[i][j].isRevealed && playField[i][j].minesAround == 0)
+            {
+                printf("□ ");
+            }
+            if (playField[i][j].isRevealed && playField[i][j].isFlagged)
+            {
+                printf("F ");
+            }
+            if (playField[i][j].isRevealed && playField[i][j].minesAround != 0)
+            {
+                printf("%d ", playField[i][j].minesAround);
+            }
+        }
+        printf("\n");
+    }
 }
