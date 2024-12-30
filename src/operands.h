89654cbefc721a30aedb8a0ad5b831a6fad9a300
kzzs <01197783@pw.edu.pl>
2024-12-30 19:37:31 +0100

    dodanie naglowkow w dodanych funkcjach z operands.c

--- src/operands.h
+++ src/operands.h
@@ -14,8 +14,12 @@ tile **createTiles(int width, int height);
 
 void printBoard(int height, int width, tile **playField);
 
-void placeMines(int width, int height, int mines, tile **playField);
+void placeMines(int width, int height, int mines, tile **playField, int x, int y);
 
 void revealMines(int width, int height, tile **playField);
+
+int checkGameStatus(int width, int height, int mines,  tile **playField);
+
+void printFullBoard(int height, int width, tile **playField);
 
 #endif
