625c3927a3984d3a4a3f00a00e34cc6a66b51180
kzzs <01197783@pw.edu.pl>
2024-12-30 15:03:38 +0100

    naglowki funkcji operands.c

--- src/operands.h
+++ src/operands.h
@@ -4,12 +4,18 @@
 #include <stdbool.h>
 
 typedef struct tile{
-    int x, y;
     bool isFlagged;
     bool isMine;
+    bool isRevealed;
     int minesAround;
 }tile;
 
-tile *createTiles(int width, int height);
+tile **createTiles(int width, int height);
+
+void printBoard(int height, int width, tile **playField);
+
+void placeMines(int width, int height, int mines, tile **playField);
+
+void revealMines(int width, int height, tile **playField);
 
 #endif
